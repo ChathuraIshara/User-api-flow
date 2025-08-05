6c6df00aa4cccd5f0c093541968722bd8b3c8ee3
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2025-08-05 14:01:53 +0530

    Refactor CORS configuration to allow all origins and reorder middleware setup

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ dotenv.config();// For loading env values to the nodejs process,then we can acce
 connectDb(); // Connect to MongoDB
 
 const app = express(); //create an express app
-app.use(express.json()); // Middleware to parse JSON bodies
-
-app.use('/api/usersDemo', userRoutes); // Mount user routes
 
 const cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:5173', // React app url
+  origin: true,
   credentials: true
 }));
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use('/api/usersDemo', userRoutes); // Mount user routes
 
 const port = process.env.PORT;
 app.listen(port,()=>console.log('Server is running on port',port)); // Start the server 
